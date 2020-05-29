a092f69d4ff05f6218d37c6a42ab2036d7043785
pulkit6559 <pulkitarora7.fas@gmail.com>
2020-05-29 20:07:22 +0530

    inception block

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ class InceptionBlock(nn.Module):
             conv_block(red_3x3, out_3x3, kernel_size=3, padding=1, stride=1)
         )
         self.branch3 = nn.Sequential(
-            conv_block(in_channels, red_5x5, kernel_stride=1),
+            conv_block(in_channels, red_5x5, kernel_size=1),
             conv_block(red_5x5, out_5x5, kernel_size=5, padding=2, stride=1)
         )
         self.branch4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, padding=1, stride=1),
-            conv_block(in_channels, out_1x1, kernel_size=1)
+            conv_block(in_channels, out_1x1pool, kernel_size=1)
         )
 
     def forward(self, x):
@@ -38,3 +38,66 @@ class InceptionBlock(nn.Module):
             self.branch3(x),
             self.branch4(x)
         ], dim=1)
+
+class InceptionNet(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000):
+        super(InceptionNet, self).__init__()
+        self.conv1 = conv_block(in_channels=in_channels, out_channels=64, kernel_size=(7,7),
+                                stride=(2,2), padding=(3,3))
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv2 = conv_block(64, 192, kernel_size=3, stride=1, padding=1)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # in_channels, out_1x1, red_3x3, out_3x3, red_5x5, out_5x5, out_1x1pool
+        self.inception3a = InceptionBlock(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = InceptionBlock(256, 128, 128, 192, 32, 96, 64)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.inception4a = InceptionBlock(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = InceptionBlock(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = InceptionBlock(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = InceptionBlock(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = InceptionBlock(528, 256, 160, 320, 32, 128, 128)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.inception5a = InceptionBlock(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = InceptionBlock(832, 384, 192, 384, 48, 128, 128)
+
+        self.avgpool = nn.AvgPool2d(kernel_size=7, stride=1)
+        self.dropout = nn.Dropout(p=0.4)
+        self.fc1 = nn.Linear(1024, 1000)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.maxpool2(x)
+
+        x = self.inception3a(x)
+        x = self.inception3b(x)
+        x = self.maxpool3(x)
+
+        x = self.inception4a(x)
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        x = self.inception4e(x)
+        x = self.maxpool4(x)
+
+        x = self.inception5a(x)
+        x = self.inception5b(x)
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+
+        return x
+
+if __name__ == '__main__':
+    x = torch.randn(3,3,224,224)
+    model = InceptionNet()
+    print(model(x).shape)
+
+
+
+
